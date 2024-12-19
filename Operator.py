1caf08052c27d2c4f8085ef202652e3ba0b9e458
Aadhancodingal <kcmharish@gmail.com>
2024-12-19 17:46:15 +0530

    initial commit

---
+++ Operator.py
@@ -0,0 +1,20 @@
+num1 = 40
+num2 = 30
+
+print("Num 1: ",num1)
+print("Num 2: ",num2)
+print("Addtion : ",num1+num2)
+print("Subtraction :",num1-num2)
+print("Mulitipication :",num1*num2)
+print("Division :",num1/num2)
+print("Floor Division :",num1//num2)
+print("Modulus :",num1%num2)
+print("Expendition:",num1**2)
+print("Square root:",num1**0.5)
+ 
+print("Equal :",num1==num2)
+print("Not Equal :",num1!=num2)
+print("Greater than :",num1>num2)
+print("Smaller than :",num1<num2)
+print("Greater than or equal to :",num1>=num2)
+print("Smaller than or equal to:",num1<=num2)
