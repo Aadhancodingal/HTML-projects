778b132efb26f5c13a62aa448093165b3c93cd83
Aadhancodingal <kcmharish@gmail.com>
2024-12-19 17:21:22 +0530

    initial commit

---
+++ Data types.py
@@ -0,0 +1,21 @@
+name = "Aadhan"
+age = 10
+weight = 26.5
+student = True
+
+print("Name : " + name)
+print("Data type of name is ",type(name))
+print("Age :", age)
+print("Data type of age is ", type(age) )
+print("Weight :", weight)
+print("Data type of weight is",type(weight))
+print("Is Student",student)
+print("Data type of student is",type(student))
+
+print("\n After Type Casting")
+age = str(age)
+print(age)
+print("Data type of age is",type(age))
+weight = int(weight)
+print(weight)
+print("Data type of age is",type(weight))   
