c5629c6601c93952841cffa70d4427f688e15f94
Aadhancodingal <kcmharish@gmail.com>
2024-12-21 18:02:43 +0530

    initial commit

---
+++ Triangle & Square.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.Screen().bgcolor("Green")
+
+board = turtle.Turtle()
+ # triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+board.penup()
+board.goto(100,150)
+board.pendown()
+# Square
+
+board.left(90)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
