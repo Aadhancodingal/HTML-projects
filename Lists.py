e995b77d1f3f169c3a4d64bc9142ad510f99b015
Aadhancodingal <kcmharish@gmail.com>
2024-12-22 12:12:55 +0530

    initial commit

---
+++ Lists.py
@@ -0,0 +1,21 @@
+lst=["apple","mango","papaya","orange","banana"]
+lst.append("grapes")
+print("Apended List:",lst)
+lst.remove("papaya")
+print("Apended List:",lst)
+len(lst)
+
+lst = lst[0:4]
+print("Sliced List:",lst)
+
+lst.pop(1)
+print("Popped List:",lst)
+
+lst.sort()
+print("Sorted List:",lst)
+
+lst.reverse()
+print("Reveresed List:",lst)
+
+lst.clear()
+print("Cleared List:",lst)
