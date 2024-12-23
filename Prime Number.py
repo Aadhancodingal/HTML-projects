2c78a826d3b772b94f6ed742cdaa4a9332657e47
Aadhancodingal <kcmharish@gmail.com>
2024-12-23 17:13:01 +0530

    initial commit

---
+++ Prime Number.py
@@ -0,0 +1,12 @@
+num =int(input("Enter a number"))
+flag = False
+if(num>1):
+# check for factors
+ for i in range(2,num):
+  if(num%i==0):
+   flag = True
+   break
+if (flag):
+ print("It is not a prime Number")
+else:
+ print("It is a prime number")    
