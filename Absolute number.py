a1ce67367d6f2707a9e3f5acc5645c224878acb8
Aadhancodingal <kcmharish@gmail.com>
2025-01-02 17:13:20 +0530

    initial commit

---
+++ Absolute number.py
@@ -0,0 +1,8 @@
+def absolute(num):
+  if(num>=0):   
+   return num
+  else:
+   return -num
+
+print("The Absoulte value of 17 is " ,absolute(17))
+print("The Absoulte value of -135 is " ,absolute(-135))    
