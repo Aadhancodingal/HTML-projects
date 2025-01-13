34c1c210245f71d134abe9cc5493df44b33f5205
Aadhancodingal <kcmharish@gmail.com>
2025-01-13 11:36:44 +0530

    initial commit

--- Phonebook.py
+++ Phonebook.py
@@ -26,4 +26,31 @@ def initial_phonebook():
     if (j==4):
      temp.append(str(input("Enter address:")))
      if(temp[j]==''or temp[j==' ']):
-      temp[j=='None']    
+      temp[j=='None']   
+    phonebook.append(temp)
+def menu():  
+  print("***************************************************************************")
+  print("\t\t\tSMARTPHONE DICTIONARY",flush=False)
+  print("***************************************************************************")
+  print("\tYou can now perform the following operations gven on this phone book\n")
+  print("1. Add a new contact")
+  print("2. Remove an existing contact")
+  print("3. Delete all contacts")
+  print("4. Search for a contact")
+  print("5. Display all contacts")
+  print("6. Exit phonebook")
+
+  choice = int(input("Please enter your choice"))
+  return choice
+
+def add_contact(pb):
+ dip = []
+ for i in range(len(pb[0])):
+   if (i==0):
+    dip.append(str(input("Enter name :"))) 
+   if (i==1):
+    dip.append(int(input("Enter number :")))
+   if (i==2):
+    dip.append(str(input("Enter e-mail address :")))
+   if (i==3):
+    dip.append(str(input("Enter date of Birth(dd/mm/yy) :")))
